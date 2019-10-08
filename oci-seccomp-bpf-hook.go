58d48806805d7c2cfc0c1a8fa68e9589f4437e89
Valentin Rothberg <rothberg@redhat.com>
2019-10-08 16:07:26 +0200

    refactor into `ebpf.go`
    Refactor parts of the code, the raw source string in particular, into a
    dedicated `ebpf.go` to make the code a bit more approachable.
    
    Signed-off-by: Valentin Rothberg <rothberg@redhat.com>

--- oci-seccomp-bpf-hook.go
+++ oci-seccomp-bpf-hook.go
@@ -20,7 +20,7 @@ import (
 	"time"
 
 	"github.com/docker/docker/api/types"
-	"github.com/iovisor/gobpf/bcc" //nolint
+	"github.com/iovisor/gobpf/bcc"
 	spec "github.com/opencontainers/runtime-spec/specs-go"
 	seccomp "github.com/seccomp/libseccomp-golang"
 	"github.com/sirupsen/logrus"
@@ -30,100 +30,6 @@ import (
 // ebpfTimout is the timeout in seconds to wait for the child process to signal
 // that the eBPF program finished compiling and attached to the tracee.
 const ebpfTimeout = 10
-
-// event struct used to read data from the perf ring buffer
-type event struct {
-	// PID of the process making the syscall
-	Pid uint32
-	// syscall number
-	ID uint32
-	// Command which makes the syscall
-	Command [16]byte
-	// Stops tracing syscalls if true
-	StopTracing bool
-}
-
-// the source is a bpf program compiled at runtime. Some macro's like
-// BPF_HASH and BPF_PERF_OUTPUT are expanded during compilation
-// by bcc. $PARENT_PID get's replaced before compilation with the PID of the container
-// Complete documentation is available at
-// https://github.com/iovisor/bcc/blob/master/docs/reference_guide.md
-const source string = `
-#include <linux/bpf.h>
-#include <linux/nsproxy.h>
-#include <linux/pid_namespace.h>
-#include <linux/ns_common.h>
-#include <linux/sched.h>
-#include <linux/tracepoint.h>
-
-// BPF_HASH used to store the PID namespace of the parent PID
-// of the processes inside the container.
-BPF_HASH(parent_namespace, u64, unsigned int);
-
-// Opens a custom BPF table to push data to user space via perf ring buffer
-BPF_PERF_OUTPUT(events);
-
-// data_t used to store the data received from the event
-struct syscall_data {
-    // PID of the process
-    u32 pid;
-    // the syscall number
-    u32 id;
-    // command which is making the syscall
-    char comm[16];
-    // Stops tracing syscalls if true
-    bool stopTracing;
-};
-
-// enter_trace : function is attached to the kernel tracepoint raw_syscalls:sys_enter it is
-// called whenever a syscall is made. The function stores the pid_namespace (task->nsproxy->pid_ns_for_children->ns.inum) of the PID which
-// starts the container in the BPF_HASH called parent_namespace.
-// The data of the syscall made by the process with the same pid_namespace as the parent_namespace is pushed to
-// userspace using perf ring buffer
-
-// specification of args from sys/kernel/debug/tracing/events/raw_syscalls/sys_enter/format
-int enter_trace(struct tracepoint__raw_syscalls__sys_enter* args)
-{
-    struct syscall_data data = {};
-    u64 key = 0;
-    unsigned int zero = 0;
-    struct task_struct* task;
-
-    data.pid = bpf_get_current_pid_tgid();
-    data.id = (int)args->id;
-    bpf_get_current_comm(&data.comm, sizeof(data.comm));
-
-    task = (struct task_struct*)bpf_get_current_task();
-    struct nsproxy* ns = task->nsproxy;
-    unsigned int inum = ns->pid_ns_for_children->ns.inum;
-
-    if (data.pid == $PARENT_PID) {
-        parent_namespace.update(&key, &inum);
-    }
-    unsigned int* parent_inum = parent_namespace.lookup_or_init(&key, &zero);
-
-    if (*parent_inum != inum) {
-        return 0;
-    }
-
-    data.stopTracing = false;
-    events.perf_submit(args, &data, sizeof(data));
-    return 0;
-}
-
-// Checks if the container has exited
-int check_exit(struct tracepoint__sched__sched_process_exit* args)
-{
-    if (args->pid == $PARENT_PID) {
-        struct syscall_data data = {};
-        data.pid = args->pid;
-        data.id = 0;
-        data.stopTracing = true;
-        events.perf_submit(args, &data, sizeof(data));
-    }
-    return 0;
-}
-`
 
 // version is the version string of the hook. Set at build time.
 var version string
@@ -164,14 +70,15 @@ func main() {
 			logrus.Fatal(err)
 		}
 	} else if *start {
-		if err := startFloatingProcess(); err != nil {
+		if err := detachAndTrace(); err != nil {
 			logrus.Fatal(err)
 		}
 	}
 }
 
-// Start a process which runs the BPF source and detach the process
-func startFloatingProcess() error {
+// detachAndTrace re-executes the current executable to "fork" in go-ish way and
+// traces the provided PID.
+func detachAndTrace() error {
 	var s spec.State
 	reader := bufio.NewReader(os.Stdin)
 	decoder := json.NewDecoder(reader)
