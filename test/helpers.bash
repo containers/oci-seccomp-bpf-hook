a5a5abf832813628343b2da878da231dccc82755
Wei <wei@hardenedlinux.org>
2024-09-07 18:18:47 +1000

    Make the test work w/ non-default hooks directory
    By default the hook is expected to locate in
    /usr/share/containers/oci/hooks.d
    However, "make install" uses the prefix /usr/local as default. This
    change enables "make test-integration" to work in this situation.
    
    Signed-off-by: Wei <wei@hardenedlinux.org>

--- test/helpers.bash
+++ test/helpers.bash
@@ -3,3 +3,9 @@ BLOCK_MKDIR=$(realpath $(dirname ${BASH_SOURCE[0]})/fixtures/block-mkdir.json)
 
 # Hostname that should be ping'able from any environment in which we run tests
 PINGABLE_HOST=github.com
+
+# Without giving HOOKS_DIR, the option will not appear, which avoids writing
+# default hooks directory in the command.
+if [ ! -z $HOOKS_DIR ]; then
+	HOOKS_DIR_OPT="--hooks-dir=$HOOKS_DIR"
+fi
